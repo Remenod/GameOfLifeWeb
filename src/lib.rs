e2d2f444bfd1afe8c3c3ba906babba9588e2a3be
Remenod <remenod@gmail.com>
2025-06-10 19:02:27 +0300

    add base game logic

--- src/lib.rs
+++ src/lib.rs
@@ -1,14 +1,54 @@
-pub fn add(left: u64, right: u64) -> u64 {
-    left + right
+use wasm_bindgen::prelude::*;
+
+const WIDTH: usize = 100;
+const HEIGHT: usize = 100;
+const VOLUME: usize = WIDTH * HEIGHT;
+
+const OFFSETS: [(isize, isize); 8] = [
+    (-1, -1),
+    (-1, 0),
+    (-1, 1),
+    (0, -1),
+    (0, 1),
+    (1, -1),
+    (1, 0),
+    (1, 1),
+];
+
+fn get_index(x: usize, y: usize) -> usize {
+    y * WIDTH + x
 }
 
-#[cfg(test)]
-mod tests {
-    use super::*;
+fn get_x_y(index: usize) -> (usize, usize) {
+    let y = index / WIDTH;
+    let x = index % WIDTH;
+    (x, y)
+}
 
-    #[test]
-    fn it_works() {
-        let result = add(2, 2);
-        assert_eq!(result, 4);
+fn check_next_turn(data: &Vec<bool>, index: usize) -> bool {
+    let (x, y) = get_x_y(index);
+
+    let neighbours: u8 = OFFSETS
+        .iter()
+        .map(|(dx, dy)| (x.wrapping_add_signed(*dx), y.wrapping_add_signed(*dy)))
+        .filter(|(nx, ny)| *nx < WIDTH && *ny < HEIGHT)
+        .map(|(nx, ny)| data[get_index(nx, ny)] as u8)
+        .sum();
+
+    match neighbours {
+        2 => data[index],
+        //0 | 1 | 4..=8 => false,
+        3 => true,
+        _ => false,
+    }
+}
+
+#[wasm_bindgen]
+pub fn main() {
+    let mut curr_field = vec![false; VOLUME.into()];
+    let mut prev_field = curr_field.clone();
+
+    for i in 0..VOLUME {
+        curr_field[i] = check_next_turn(&prev_field, i);
     }
 }
