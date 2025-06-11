1ca93d701ea1467f761884f633a048c08acf8321
Remenod <Remenod@users.noreply.github.com>
2025-06-11 18:01:43 +0000

    deploy: a4ea7547a32c3ee4786e80d9be82089a9293d9c4

---
+++ pkg/game_of_life.d.ts
@@ -0,0 +1,48 @@
+/* tslint:disable */
+/* eslint-disable */
+export class WasmGame {
+  free(): void;
+  constructor(width: number, height: number, field: Uint8Array, rule: string);
+  tick(): void;
+  get_cell(x: number, y: number): boolean;
+  set_cell(x: number, y: number, value: boolean): void;
+  export_field(): string;
+}
+
+export type InitInput = RequestInfo | URL | Response | BufferSource | WebAssembly.Module;
+
+export interface InitOutput {
+  readonly memory: WebAssembly.Memory;
+  readonly __wbg_wasmgame_free: (a: number, b: number) => void;
+  readonly wasmgame_new: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+  readonly wasmgame_tick: (a: number) => void;
+  readonly wasmgame_get_cell: (a: number, b: number, c: number) => number;
+  readonly wasmgame_set_cell: (a: number, b: number, c: number, d: number) => void;
+  readonly wasmgame_export_field: (a: number) => [number, number];
+  readonly __wbindgen_export_0: WebAssembly.Table;
+  readonly __wbindgen_malloc: (a: number, b: number) => number;
+  readonly __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+  readonly __wbindgen_free: (a: number, b: number, c: number) => void;
+  readonly __wbindgen_start: () => void;
+}
+
+export type SyncInitInput = BufferSource | WebAssembly.Module;
+/**
+* Instantiates the given `module`, which can either be bytes or
+* a precompiled `WebAssembly.Module`.
+*
+* @param {{ module: SyncInitInput }} module - Passing `SyncInitInput` directly is deprecated.
+*
+* @returns {InitOutput}
+*/
+export function initSync(module: { module: SyncInitInput } | SyncInitInput): InitOutput;
+
+/**
+* If `module_or_path` is {RequestInfo} or {URL}, makes a request and
+* for everything else, calls `WebAssembly.instantiate` directly.
+*
+* @param {{ module_or_path: InitInput | Promise<InitInput> }} module_or_path - Passing `InitInput` directly is deprecated.
+*
+* @returns {Promise<InitOutput>}
+*/
+export default function __wbg_init (module_or_path?: { module_or_path: InitInput | Promise<InitInput> } | InitInput | Promise<InitInput>): Promise<InitOutput>;
---
+++ pkg/game_of_life_bg.wasm.d.ts
@@ -0,0 +1,14 @@
+/* tslint:disable */
+/* eslint-disable */
+export const memory: WebAssembly.Memory;
+export const __wbg_wasmgame_free: (a: number, b: number) => void;
+export const wasmgame_new: (a: number, b: number, c: number, d: number, e: number, f: number) => number;
+export const wasmgame_tick: (a: number) => void;
+export const wasmgame_get_cell: (a: number, b: number, c: number) => number;
+export const wasmgame_set_cell: (a: number, b: number, c: number, d: number) => void;
+export const wasmgame_export_field: (a: number) => [number, number];
+export const __wbindgen_export_0: WebAssembly.Table;
+export const __wbindgen_malloc: (a: number, b: number) => number;
+export const __wbindgen_realloc: (a: number, b: number, c: number, d: number) => number;
+export const __wbindgen_free: (a: number, b: number, c: number) => void;
+export const __wbindgen_start: () => void;
