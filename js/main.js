ac9b0edc3ec126a15a30740fe817665ad61873d0
Remenod <remenod@gmail.com>
2025-06-23 19:36:48 +0300

    copy template buttons

--- js/main.js
+++ js/main.js
@@ -3,15 +3,12 @@ import { copyUrl, resetNeighborMask } from "./form.js";
 import { loadFromUrlParams } from "./utils.js";
 import { initToggledCellsCollection, drawPreviewCanvas } from "./canvas.js";
 import { enforceDisabledControls, resetElementValue, openHelp } from "./utils.js";
-import { togglePlay, copyField_v1, copyField_v2, copyField_v3, tick, clearGrid } from "./game.js";
+import { togglePlay, tick, clearGrid } from "./game.js";
 
 enforceDisabledControls();
 loadFromUrlParams();
 
 window.tick = tick;
-window.copyField_v1 = copyField_v1;
-window.copyField_v2 = copyField_v2;
-window.copyField_v3 = copyField_v3;
 window.copyUrl = copyUrl;
 window.togglePlay = togglePlay;
 window.resetNeighborMask = resetNeighborMask;
