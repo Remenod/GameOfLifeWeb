4025a14f33bccaf7062e18b7efb8216fc6c05d81
Remenod <Remenod@users.noreply.github.com>
2025-06-23 17:46:36 +0000

    deploy: 89e00e896e4e7c5a7c68f9f1dd23ed767fd48f7f

--- js/main.js
+++ js/main.js
@@ -1,23 +1,10 @@
 import init from "../pkg/game_of_life.js";
-import { copyUrl, resetNeighborMask } from "./form.js";
-import { loadFromUrlParams } from "./utils.js";
+import "./form.js";
 import { initToggledCellsCollection, drawPreviewCanvas } from "./canvas.js";
-import { enforceDisabledControls, resetElementValue, openHelp } from "./utils.js";
-import { togglePlay, copyField_v1, copyField_v2, copyField_v3, tick, clearGrid } from "./game.js";
+import { enforceDisabledControls, loadFromUrlParams } from "./utils.js";
 
-enforceDisabledControls();
 loadFromUrlParams();
-
-window.tick = tick;
-window.copyField_v1 = copyField_v1;
-window.copyField_v2 = copyField_v2;
-window.copyField_v3 = copyField_v3;
-window.copyUrl = copyUrl;
-window.togglePlay = togglePlay;
-window.resetNeighborMask = resetNeighborMask;
-window.clearGrid = clearGrid;
-window.resetElementValue = resetElementValue;
-window.openHelp = openHelp;
+enforceDisabledControls();
 
 await init().then(() => {
     drawPreviewCanvas();
