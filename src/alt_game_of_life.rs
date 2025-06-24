105fa915a7d8db5e0e81c396d18c79c6b303295b
Remenod <remenod@gmail.com>
2025-06-24 22:26:29 +0300

    impl `next_turn`

--- src/alt_game_of_life.rs
+++ src/alt_game_of_life.rs
@@ -1,10 +1,10 @@
 use crate::{
     game_rule::GameRule,
-    traits::game::Game,
+    traits::{field_acces::FieldAccess, game::Game},
     utils::{check_cell_next_turn::*, coord_converter::*},
 };
 use once_cell::sync::Lazy;
-use rustc_hash::FxHashSet;
+use rustc_hash::{FxBuildHasher, FxHashSet};
 
 struct AltGameOfLife {
     width: usize,
@@ -12,9 +12,12 @@ struct AltGameOfLife {
     total_cells: usize,
     current_field: FxHashSet<usize>,
     next_field: FxHashSet<usize>,
+    empty_to_check: FxHashSet<usize>,
     game_rule: GameRule,
     offsets: Vec<(i8, i8)>,
 }
+
+static FULL_OFFSETS: Lazy<Vec<(i8, i8)>> = Lazy::new(|| GameRule::get_offsets(&vec![1; 25]));
 
 impl Game for AltGameOfLife {
     fn new(
@@ -24,24 +27,73 @@ impl Game for AltGameOfLife {
         game_rule: GameRule,
         check_rule: &[u8],
     ) -> AltGameOfLife {
+        let offsets = GameRule::get_offsets(check_rule);
         AltGameOfLife {
             width: width,
             height: height,
             total_cells: width * height,
+            next_field: FxHashSet::with_capacity_and_hasher(
+                current.len(),
+                FxBuildHasher::default(),
+            ),
+            empty_to_check: FxHashSet::with_capacity_and_hasher(
+                current.len() * offsets.len(),
+                FxBuildHasher::default(),
+            ),
             current_field: current
                 .into_iter()
                 .enumerate()
                 .filter(|(_, x)| *x != 0)
                 .map(|(i, _)| i)
                 .collect(),
-            next_field: FxHashSet::default(),
             game_rule: game_rule,
-            offsets: GameRule::get_offsets(check_rule),
+            offsets: offsets,
         }
     }
 
     fn next_turn(&mut self) {
-        todo!()
+        self.empty_to_check.clear();
+        self.next_field.clear();
+
+        for cell in &self.current_field {
+            if check_cell_next_turn(
+                &self.current_field,
+                &self.game_rule,
+                *cell,
+                self.width,
+                self.height,
+                &self.offsets,
+            ) {
+                self.next_field.insert(*cell);
+            }
+
+            let (x, y) = get_x_y(self.width, *cell);
+            for (dx, dy) in FULL_OFFSETS.iter() {
+                let nx = x.wrapping_add_signed(*dx as isize);
+                let ny = y.wrapping_add_signed(*dy as isize);
+
+                if nx < self.width && ny < self.height {
+                    let index = get_index(self.width, nx, ny);
+                    if !self.current_field.is_alive(index) {
+                        self.empty_to_check.insert(index);
+                    }
+                }
+            }
+        }
+
+        for cell in &self.empty_to_check {
+            if check_cell_next_turn(
+                &self.current_field,
+                &self.game_rule,
+                *cell,
+                self.width,
+                self.height,
+                &self.offsets,
+            ) {
+                self.next_field.insert(*cell);
+            }
+        }
+        self.current_field = std::mem::take(&mut self.next_field);
     }
 
     fn get_cell(&self, x: usize, y: usize) -> bool {
