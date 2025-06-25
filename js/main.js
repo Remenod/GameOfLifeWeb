979ce1ec70cb5c5222650a10747a14dbaaa015b2
Remenod <117807650+Remenod@users.noreply.github.com>
2025-06-26 02:47:21 +0300

    Revert "Mask url arg (#22)"
    This reverts commit fc3c13f4dbad915ce5ec790128e85116515cef41.

--- js/main.js
+++ js/main.js
@@ -1,13 +1,12 @@
 import init from "../pkg/game_of_life.js";
 import "./form.js";
 import { initToggledCellsCollection, drawPreviewCanvas } from "./canvas.js";
-import { enforceDisabledControls, loadFromUrlParams, setWasmReady } from "./utils.js";
+import { enforceDisabledControls, loadFromUrlParams } from "./utils.js";
 
 loadFromUrlParams();
 enforceDisabledControls();
 
 await init().then(() => {
-    setWasmReady()
     drawPreviewCanvas();
     initToggledCellsCollection();
 });
