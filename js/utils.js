fc3c13f4dbad915ce5ec790128e85116515cef41
Remenod <117807650+Remenod@users.noreply.github.com>
2025-06-26 02:41:02 +0300

    Mask url arg (#22)
    * add `m` param
    
    * url editors async pull

--- js/utils.js
+++ js/utils.js
@@ -1,8 +1,27 @@
-export function updateUrlParams() {
+import { setNeighborMask, getNeighborMask } from "./form.js";
+import { decode_field, encode_field } from "../pkg/game_of_life.js";
+
+let wasmIsReady = false;
+let wasmReadyResolvers = [];
+
+export function setWasmReady() {
+    wasmIsReady = true;
+    wasmReadyResolvers.forEach(fn => fn());
+    wasmReadyResolvers = [];
+}
+
+async function waitForWasm() {
+    if (wasmIsReady) return;
+    await new Promise(resolve => wasmReadyResolvers.push(resolve));
+}
+
+export async function updateUrlParams() {
+    await waitForWasm();
     const width = document.getElementById("widthInput").value;
     const height = document.getElementById("heightInput").value;
     const rule = document.getElementById("ruleInput").value;
     const field = document.getElementById("fieldInput").value;
+    const mask = getNeighborMask();
 
     const params = new URLSearchParams();
 
@@ -10,23 +29,27 @@ export function updateUrlParams() {
     if (height) params.set("h", height);
     if (rule) params.set("r", rule.replace("/", "."));
     if (field) params.set("f", field.replace(/\[/g, '-').replace(/\]/g, '_').replace(";", '.'));
+    if (mask) params.set("m", encode_field(mask.join(""), true));
 
     const newUrl = `${location.pathname}?${params.toString()}`;
     history.replaceState(null, "", newUrl);
 }
 
-export function loadFromUrlParams() {
+export async function loadFromUrlParams() {
+    await waitForWasm();
     const params = new URLSearchParams(window.location.search);
 
     const width = params.get("w");
     const height = params.get("h");
     const rule = params.get("r");
     const field = params.get("f");
+    const mask = params.get("m");
 
     if (width) document.getElementById("widthInput").value = width;
     if (height) document.getElementById("heightInput").value = height;
     if (rule) document.getElementById("ruleInput").value = rule.replace(".", "/");
     if (field) document.getElementById("fieldInput").value = field.replace(/-/g, '[').replace(/_/g, "]").replace(".", ";");
+    if (mask) setNeighborMask(decode_field(mask, true));
 }
 
 export function enforceDisabledControls() {
